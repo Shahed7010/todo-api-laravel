308b8e0081448cb0d88577db544b18742d076865
Shaheduzzaman Shahed <shahed291prof@gmail.com>
2020-04-06 19:11:29 +0600

    todo api done

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+           'title' => 'required|string'
+        ]);
+        $todo = Todo::create($data);
+        return response($todo, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $data = $request->validate([
+            'title' => 'required|string',
+            'completed' => 'required|boolean'
+        ]);
+        $todo->update($data);
+        return response($todo, 200);
+
+    }
+    public function markCompleted(Request $request)
+    {
+        $data = $request->validate([
+            'completed' => 'required|boolean'
+        ]);
+        Todo::query()->update($data);
+        return response('updated', 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        if ($todo->delete()){
+         return response('deleted successfully', 200);
+        }
+
+    }
+    public function destroyCompleted($request)
+    {
+//        $request->validate([
+//            'todos' => 'required|array'
+//        ]);
+        $todos = explode(',',$request);
+        Todo::destroy($todos);
+         return response($todos, 200);
+
+    }
+}
